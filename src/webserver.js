00c3d59703983f5b97c801abfc5f79c30c5a385b
Dustin Hilgaertner <dhilgaertner@gmail.com>
2014-03-09 15:46:56 -0400

    - fixing some stuff for heroku - also removing restriction of logging in before seeing game

--- src/webserver.js
+++ src/webserver.js
@@ -446,8 +446,8 @@ module.exports.server = server;
 
 		// Basic Routes (entirely client-side parsed, goal is to move the rest of the crap in this file into this one section)
 		(function () {
-			var routes = ['login', 'register', 'account', 'recent', 'popular', '403', '404', '500'],
-				loginRequired = ['unread', 'notifications', 'game'];
+			var routes = ['login', 'register', 'account', 'recent', 'popular', 'game', '403', '404', '500'],
+				loginRequired = ['unread', 'notifications'];
 
 			async.each(routes.concat(loginRequired), function(route, next) {
 				app.get('/' + route, function (req, res) {
