72b3ed12aba030604bf9a5f7e84687a868df42ae
Dustin Hilgaertner <dhilgaertner@gmail.com>
2014-03-09 17:25:13 -0400

    - mode fiddling

--- public/src/game/main.js
+++ public/src/game/main.js
@@ -9,59 +9,57 @@ function GameCtrl($scope) {
     function onReceiveGameData(err, data) {
         console.dir(data);
 
-        Raphael("game-board", function(){
-            var paper = this;
+        var paper = Raphael("game-board");
 
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-                paper.setSize(700,480);
-            } else {
-                paper.setViewBox(0,0,700,480,true);
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            paper.setSize(700,480);
+        } else {
+            paper.setViewBox(0,0,700,480,true);
+        }
+
+        // ok, raphael sets width/height even though a viewBox has been set, so let's rip out those attributes (yes, this will not work for VML)
+        var svg = document.querySelector("svg");
+        svg.removeAttribute("width");
+        svg.removeAttribute("height");
+
+        paper.setStart();
+        data.borders.forEach(function(border){
+            paper.path(border.path);
+        });
+        var outsideBorder = paper.setFinish();
+        outsideBorder.blur(10);
+
+        paper.setStart();
+        data.lands.forEach(function(land){
+            var path = paper.path(land.path);
+            path.attr({fill: '#D3E2DA','stroke-width': '.5','stroke-opacity': '1'});
+
+            if (land.transform){
+                path.transform(land.transform);
             }
+        });
+        var lands = paper.setFinish();
 
-            // ok, raphael sets width/height even though a viewBox has been set, so let's rip out those attributes (yes, this will not work for VML)
-            var svg = document.querySelector("svg");
-            svg.removeAttribute("width");
-            svg.removeAttribute("height");
+        lands.hover(function(){ //IN
+            this.a = {
+                fill: this.attr('fill'),
+                opacity: this.attr('opacity')
+            };
 
-            paper.setStart();
-            data.borders.forEach(function(border){
-                paper.path(border.path);
-            });
-            var outsideBorder = paper.setFinish();
-            outsideBorder.blur(10);
-
-            paper.setStart();
-            data.lands.forEach(function(land){
-                var path = paper.path(land.path);
-                path.attr({fill: '#D3E2DA','stroke-width': '.5','stroke-opacity': '1'});
-
-                if (land.transform){
-                    path.transform(land.transform);
-                }
-            });
-            var lands = paper.setFinish();
-
-            lands.hover(function(){ //IN
-                this.a = {
-                    fill: this.attr('fill'),
-                    opacity: this.attr('opacity')
-                };
-
-                this.attr({
-                    fill: 'red',
-                    opacity: 0.6
-                });
-
-            }, function(){ //OUT
-                this.attr({
-                    fill: this.a.fill,
-                    opacity: this.a.opacity
-                });
+            this.attr({
+                fill: 'red',
+                opacity: 0.6
             });
 
-            lands.click(function(){
-                alert(this.data().id);
+        }, function(){ //OUT
+            this.attr({
+                fill: this.a.fill,
+                opacity: this.a.opacity
             });
+        });
+
+        lands.click(function(){
+            alert(this.data().id);
         });
     }
 }
