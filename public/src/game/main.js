815edb4244524977b2f58490415f885c06b7d991
Dustin Hilgaertner <dhilgaertner@gmail.com>
2014-03-12 00:22:15 -0400

    - added colored pieces and text

--- public/src/game/main.js
+++ public/src/game/main.js
@@ -56,7 +56,8 @@ function GameCtrl($scope) {
         paper.setStart();
         data.army.paths.forEach(function(item){
             var path = paper.path(item.path);
-            paper.text(15,5,"100");
+            var text = paper.text(15,5,"");
+
             if (item.transform){
                 path.transform(item.transform);
             }
@@ -66,17 +67,32 @@ function GameCtrl($scope) {
             }
         });
         var army = paper.setFinish();
-        army.clone().transform("t120,85...").attr({text:"100"});
-        army.clone().transform("t80,157...");
-        army.clone().transform("t46,226...");
-        army.clone().transform("t101,218...");
-        army.clone().transform("t137,184...");
-        army.clone().transform("t180,94...");
-        army.clone().transform("t186,181...");
-        army.clone().transform("t141,245...");
-        army.clone().transform("t236,118...");
-        army.clone().transform("t242,215...");
-        army.clone().transform("t310,141...");
+        army.clone().transform("t120,85...").attr({text:"1"});
+        army.clone().transform("t80,157...").attr({text:"2"});
+        army.clone().transform("t46,226...").attr({text:"3"});
+        army.clone().transform("t101,218...").attr({text:"4"});
+        army.clone().transform("t137,184...").attr({text:"5"});
+        army.clone().transform("t180,94...").attr({text:"6"});
+        army.clone().transform("t186,181...").attr({text:"7"});
+        army.clone().transform("t141,245...").attr({text:"8"});
+        army.clone().transform("t236,118...").attr({text:"9"});
+        army.clone().transform("t242,215...").attr({text:"10"});
+        army.clone().transform("t310,141...").attr({text:"11"});
+        army.clone().transform("t359,87.3...").attr({text:"12"});
+        army.clone().transform("t427,100...").attr({text:"13"});
+        army.clone().transform("t424,169...").attr({text:"14"});
+        army.clone().transform("t337,200...").attr({text:"15"});
+        army.clone().transform("t378,241...").attr({text:"16"});
+        army.clone().transform("t302,305...").attr({text:"17"});
+        army.clone().transform("t365,301...").attr({text:"18"});
+        army.clone().transform("t429,333...").attr({text:"19"});
+        army.clone().transform("t455,238...").attr({text:"20"});
+        army.clone().transform("t522,169...").attr({text:"21", fill: "green"});
+        army.clone().transform("t610,191...").attr({text:"22", fill: "yellow"});
+        army.clone().transform("t573,247...").attr({text:"23", fill: "red"});
+        army.clone().transform("t506,300...").attr({text:"24", fill: "teal"});
+        army.clone().transform("t451,413...").attr({text:"25", fill: "blue"});
+        army.hide();
 
         lands.hover(function(){ //IN
             this.a = {
@@ -101,6 +117,7 @@ function GameCtrl($scope) {
         });
 
         var rect = paper.rect(0,0,700,480);
+        rect.toBack();
         rect.attr({fill: "green", opacity:.3});
         rect.mousedown(function (event, a, b) {
             // get bounding rect of the paper
@@ -118,7 +135,10 @@ function GameCtrl($scope) {
             fx = Number(fx).toPrecision(3);
             fy = Number(fy).toPrecision(3);
 
-            console.dir({x: fx, y: fy});
+            var sx = fx - 15;
+            var sy = fy - 10;
+
+            console.log("t" + sx.toString() + "," + sy.toString());
         });
     }
 }
