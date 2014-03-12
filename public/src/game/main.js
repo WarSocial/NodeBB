de6d7213324413ade9f23331f0c43f50d6d2a421
Dustin Hilgaertner <dhilgaertner@gmail.com>
2014-03-11 23:34:44 -0400

    - added game pieces and positioning

--- public/src/game/main.js
+++ public/src/game/main.js
@@ -41,6 +41,7 @@ function GameCtrl($scope) {
         paper.setStart();
         data.lands.forEach(function(land){
             var path = paper.path(land.path);
+            path.data("name", land.name);
 
             if (land.transform){
                 path.transform(land.transform);
@@ -51,6 +52,31 @@ function GameCtrl($scope) {
             }
         });
         var lands = paper.setFinish();
+
+        paper.setStart();
+        data.army.paths.forEach(function(item){
+            var path = paper.path(item.path);
+
+            if (item.transform){
+                path.transform(item.transform);
+            }
+
+            if (item.attr){
+                path.attr(item.attr);
+            }
+        });
+        var army = paper.setFinish();
+        army.clone().transform("t120,85...");
+        army.clone().transform("t80,157...");
+        army.clone().transform("t46,226...");
+        army.clone().transform("t101,218...");
+        army.clone().transform("t137,184...");
+        army.clone().transform("t180,94...");
+        army.clone().transform("t186,181...");
+        army.clone().transform("t141,245...");
+        army.clone().transform("t236,118...");
+        army.clone().transform("t242,215...");
+        army.clone().transform("t310,141...");
 
         lands.hover(function(){ //IN
             this.a = {
@@ -68,6 +94,31 @@ function GameCtrl($scope) {
                 fill: this.a.fill,
                 opacity: this.a.opacity
             });
+        });
+
+        lands.click(function(){
+            alert(this.data("name"));
+        });
+
+        var rect = paper.rect(0,0,700,480);
+        rect.attr({fill: "green", opacity:.3});
+        rect.mousedown(function (event, a, b) {
+            // get bounding rect of the paper
+            var bnds = event.target.getBoundingClientRect();
+
+            // adjust mouse x/y
+            var mx = event.clientX - bnds.left;
+            var my = event.clientY - bnds.top;
+
+            // divide x/y by the bounding w/h to get location %s and apply factor by actual paper w/h
+            var fx = mx/bnds.width * rect.attrs.width
+            var fy = my/bnds.height * rect.attrs.height
+
+            // cleanup output
+            fx = Number(fx).toPrecision(3);
+            fy = Number(fy).toPrecision(3);
+
+            console.dir({x: fx, y: fy});
         });
     }
 }
